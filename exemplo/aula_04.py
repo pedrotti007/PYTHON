a0730ff27f5614a2ea5368defe5ffe9d4a077ac7
pedrotti007 <opedrotti007@gmail.com>
2025-04-04 17:21:08 -0300

    exemplos

---
+++ exemplo/aula_04.py
@@ -0,0 +1,16 @@
+
+idade = int(input("Digite a sua idade:  "))
+
+print(f'A sua idade é? {idade}')
+
+if idade >= 18:
+    print("Aproveita visse, tenta não morrer (Opcional)")
+
+elif 16 <= idade < 18:
+    print("Ê melhor tu ir pra casa parceiro")
+
+else:
+    print("Vai embora estudar boy, tem aula amanhã!!!!")
+
+
+
---
+++ exemplo/ex_06.py
@@ -0,0 +1,6 @@
+valor = float(input("Digite o seu valor:  "))
+
+if valor >=10.0:
+        print(f"Maior ou igual a 10.0: {valor}")
+else:
+    print(f"Menor que 10.0: O seu valor é: {valor}")
